852996b4272b4ecd15d83c763a61834057c75207
deba <dmohapat9@gmail.com>
2021-06-08 10:34:43 +0000

    feature branch

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Hello USA");
+     Sustem.out.println("Welcome to feature branch from USA");
     }  
 }  
